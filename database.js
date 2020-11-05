f7e8cf9abc44ffb9c38a83d4c4751d3a2c8a8617
Aidan <aidan.bnc@gmail.com>
2020-11-04 19:56:17 -0500

    Implemented basic server functionality. Peer programming whole group.

---
+++ database.js
@@ -0,0 +1,84 @@
+
+
+export class DB{
+    constructor(){
+        //creates or loads database
+    }
+
+    newUser(){
+        //post so nothing has to happen for now 
+    }
+    joinEvent(){
+        //post so nothing has to happen for now 
+    }
+    userCreate(){
+        //post
+    }
+    userEdit(){
+        //post
+    }
+    userDelete(){
+        //post
+    }
+    userGetMyEvents(){
+        //get
+        //Owner Title date time location description capacity
+        let events = [
+            {"owner":"George","title":"Book club","date":"11/16/20","time":"2:20pm","location":"Dubois library","description":"Lets talk about 1984","capacity":"5/10"},
+            {"owner":"George","title":"Frisbee club","date":"11/20/20","time":"4:20pm","location":"Campus pond","description":"Lets toss the disc some.","capacity":"7/15"},
+            {"owner":"George","title":"Seltzer Sampling","date":"12/9/20","time":"10:00am","location":"New Worcester","description":"Quality inspection on the new facilities","capacity":"3/5"},
+            {"owner":"George","title":"Pokemon Go","date":"12/17/20","time":"2:20pm","location":"Isenberg","description":"We will be trying to catch Mew","capacity":"30/40"}
+        ]
+        return JSON.stringify(events);
+    }
+    userGetJoinedevents(){
+        //get
+        let events = [
+            {"owner":"Prateek","title":"311 HW2","date":"11/21/20","time":"4:00PM","location":" Dubois library","description":"Doing the HW2 homework","capacity":"7/10"},
+            {"owner":"Aidan","title":" Soccer ","date":"11/26/20","time":"4:20PM","location":"East Soccer Fields","description":"5v5 pickup soccer game","capacity":"6/10"},
+            {"owner":"John","title":"Umass Real Estate Club","date":"12/20/20","time":"2:45","location":"Isenberg","description":"Group discussion of market","capacity":"11/50"},
+            {"owner":"Gerald","title":"377 HW 3","date":"11/31/20","time":"3:40","location":"OHill oak common room","description":"HW3 discussion","capacity":"7/15"}
+
+        ]
+        return JSON.stringify(events);
+        
+    }
+    globalGetFeed(){
+        //get
+        let events = [
+            {"owner":"George","title":"Book club","date":"11/16/20","time":"2:20pm","location":"Dubois library","description":"Lets talk about 1984","capacity":"5/10"},
+            {"owner":"George","title":"Frisbee club","date":"11/20/20","time":"4:20pm","location":"Campus pond","description":"Lets toss the disc some.","capacity":"7/15"},
+            {"owner":"Prateek","title":"311 HW2","date":"11/21/20","time":"4:00PM","location":"Dubois library","description":"Doing the HW2 homework","capacity":"7/10"},
+            {"owner":"Aidan","title":" Soccer ","date":"11/26/20","time":"4:20PM","location":"East Soccer Fields","description":"5v5 pickup soccer game","capacity":"6/10"},
+            {"owner":"Gerald","title":"377 HW 3","date":"11/31/20","time":"3:40","location":"OHill","description":"HW3 discussion","capacity":"7/15"},
+            {"owner":"George","title":"Seltzer Sampling","date":"12/9/20","time":"10:00am","location":"OHill","description":"Quality inspection on the new facilities","capacity":"3/5"},
+            {"owner":"George","title":"Pokemon Go","date":"12/17/20","time":"2:20pm","location":"Isenberg","description":"We will be trying to catch Mew","capacity":"30/40"},
+            {"owner":"John","title":"Umass Real Estate Club","date":"12/20/20","time":"2:45","location":"Isenberg","description":"Group discussion of market","capacity":"11/50"}
+        ]
+        return JSON.stringify(events);
+    }
+    globalGetFeedByLocation(){
+        //get
+        let event_dict = {
+            "isenberg":[
+                {"owner":"George","title":"Pokemon Go","date":"12/17/20","time":"2:20pm","location":"Isenberg","description":"We will be trying to catch Mew","capacity":"30/40"},
+                {"owner":"John","title":"Umass Real Estate Club","date":"12/20/20","time":"2:45","location":"Isenberg","description":"Group discussion of market","capacity":"11/50"}
+            ],
+            "OHill":[
+                {"owner":"Gerald","title":"377 HW 3","date":"11/31/20","time":"3:40","location":"OHill","description":"HW3 discussion","capacity":"7/15"},
+            {"owner":"George","title":"Seltzer Sampling","date":"12/9/20","time":"10:00am","location":"OHill","description":"Quality inspection on the new facilities","capacity":"3/5"}
+            ],
+            "Dubois library":[
+                {"owner":"George","title":"Book club","date":"11/16/20","time":"2:20pm","location":"Dubois library","description":"Lets talk about 1984","capacity":"5/10"},
+                {"owner":"Prateek","title":"311 HW2","date":"11/21/20","time":"4:00PM","location":"Dubois library","description":"Doing the HW2 homework","capacity":"7/10"}
+            ],
+            "Campus pond":[
+                {"owner":"George","title":"Frisbee club","date":"11/20/20","time":"4:20pm","location":"Campus pond","description":"Lets toss the disc some.","capacity":"7/15"}
+            ],
+            "East Soccer Fields":[
+                {"owner":"Aidan","title":" Soccer ","date":"11/26/20","time":"4:20PM","location":"East Soccer Fields","description":"5v5 pickup soccer game","capacity":"6/10"}
+            ]
+        }
+        return  JSON.stringify(events);
+    }
+}
---
+++ server.js
@@ -0,0 +1,62 @@
+import {createServer} from 'http';
+import {parse} from 'url';
+import {join} from 'path';
+import {writeFile, readFileSync, existsSync} from 'fs';
+import { ESRCH } from 'constants';
+import {DB} from './database.js';
+
+let database = new DB();
+
+createServer(async (req, res) => {
+    const parsed = parse(req.url, true);
+
+    if (parsed.pathname === '/user/new') {
+        database.newUser();
+        
+    } else if (parsed.pathname === '/joinEvent') {
+        database.joinEvent();
+
+    } else if (parsed.pathname === '/user/create') {
+        database.userCreate();
+
+    } else if (parsed.pathname === '/user/edit') {
+        database.userEdit();
+
+    } else if (parsed.pathname === '/user/delete') {
+        database.userDelete();
+
+    } else if (parsed.pathname === '/user/getmyevents') {
+        res.end(database.userGetMyEvents());
+
+    } else if (parsed.pathname === '/user/getjoinedevents') {
+        res.end(database.userGetJoinedevents());
+
+    } else if (parsed.pathname === '/globalgetfeed/') {
+       res.end(database.globalGetFeed());
+
+    } else if (parsed.pathname === '/globalgetfeed/bylocation') {
+       res.end(database.globalGetFeedByLocation());
+
+    } else {
+        // If the client did not request an API endpoint, we assume we need to fetch and serve a file.
+        // This is terrible security-wise, since we don't check the file requested is in the same directory.
+        // This will do for our purposes.
+        const filename = parsed.pathname === '/' ? "index.html" : parsed.pathname.replace('/', '');
+        const path = join("client/", filename);
+        console.log("trying to serve " + path + "...");
+        if (existsSync(path)) {
+            if (filename.endsWith("html")) {
+                res.writeHead(200, {"Content-Type" : "text/html"});
+            } else if (filename.endsWith("css")) {
+                res.writeHead(200,{"Content-Type" : "text/css"});
+            } else if (filename.endsWith("js")){
+                res.writeHead(200,{"Content-Type" : "text/javascript"});
+            }
+            res.write(readFileSync(path));
+            res.end();
+        } else {
+            res.writeHead(404);
+            res.end();
+        }
+    }
+}).listen(8080);
