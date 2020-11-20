d949f274e218f74f9a89ad7a0bffc02e35a1f39e
Sharqosity <typingpalguy@gmail.com>
2020-11-19 19:20:38 -0500

    replace user_id with username, still need to add latitude and longitude

--- database.js
+++ database.js
@@ -23,36 +23,36 @@ export class DB{
             }
         }
     }
-    //is this used for anything?
-    newUser(){
-        //post so nothing has to happen for now 
-    }
-    userLogin(){
 
+    //Login and authentication stuff
+    getUser(username) {
+        return JSON.stringify(await this.connectAndRun(db => db.any("SELECT * FROM users WHERE username = $1;", [username])));
     }
     userRegister(username, salt, hash){
         await this.connectAndRun(db => db.none("INSERT INTO users VALUES($1, $2, $3);",
         [username, salt,hash]));
     }
-    joinEvent(user_id, event_id){
+
+
+    joinEvent(username, event_id){
         //add an entry in joined_events conatining the user and event id
-        await this.connectAndRun(db => db.none("INSERT INTO joined_events VALUES($1, $2);", [user_id, event_id]));
+        await this.connectAndRun(db => db.none("INSERT INTO joined_events VALUES($1, $2);", [username, event_id]));
 
     }
-    unjoinEvent(user_id,event_id){
+    unjoinEvent(username,event_id){
         //delete the joined_events entry containing the appropriate user and event id's
-        await this.connectAndRun(db => db.none("DELETE FROM joined_events WHERE user_id = $1 and event_id = $2;", [user_id, event_id]));
+        await this.connectAndRun(db => db.none("DELETE FROM joined_events WHERE username = $1 and event_id = $2;", [username, event_id]));
     }
-    userCreate(user_id, event_info){
+    userCreate(username, event_info){
         //create a new event 
         // event_info should be a dictionary with the appropriate fields for us to get the information
         //don't supply an event id, we choose assign it sequentially 
         await this.connectAndRun(db => db.none("INSERT INTO events VALUES($1, $2, $3, $4, $5, $6, $7, $8);",
-        [user_id, -1, event_info.title, event_info.date, event_info.time, event_info.location, event_info.description, event_info.capacity]));
+        [username, -1, event_info.title, event_info.date, event_info.time, event_info.location, event_info.description, event_info.capacity]));
         //we insert event_id as -1 so that we can then adjust it to be whatever the next value should be
         await this.connectAndRun(db => db.none("UPDATE events SET event_id = 1+(SELECT MAX(event_id) FROM events) WHERE event_id = -1;"));
         //now we need to update the created_events table with the appropriate value
-        await this.connectAndRun(db => db.none("INSERT INTO created_events(user_id, event_id) SELECT user_id, event_id FROM events WHERE event_id = (SELECT MAX(event_id) FROM events);"))
+        await this.connectAndRun(db => db.none("INSERT INTO created_events(username, event_id) SELECT username, event_id FROM events WHERE event_id = (SELECT MAX(event_id) FROM events);"))
     }
     userEdit(event_id, event_info){
         //edit an event
@@ -73,16 +73,10 @@ export class DB{
         return JSON.stringify(event);
     }
 
-    getUser(username) {
-        return JSON.stringify(await this.connectAndRun(db => db.any("SELECT * FROM users WHERE username = $1;", [username])));
-
-    }
-
-
-    userGetMyEvents(user_id){
+    userGetMyEvents(username){
         //get
         //Owner Title date time location description capacity
-        return JSON.stringify(await this.connectAndRun(db => db.any("SELECT * FROM events WHERE user_id = $1;",[user_id])));
+        return JSON.stringify(await this.connectAndRun(db => db.any("SELECT * FROM events WHERE username = $1;",[username])));
         const events = [
             {"eventid":1,"owner ":"George","title":"Book club","date":"11/16/20","time":"2:20pm","location":"Dubois library","description":"Lets talk about 1984","capacity":"5/10"},
             {"eventid":2,"owner":"George","title":"Frisbee club","date":"11/20/20","time":"4:20pm","location":"Campus pond","description":"Lets toss the disc some.","capacity":"7/15"},
@@ -91,9 +85,9 @@ export class DB{
         ];
         return JSON.stringify(events);
     }
-    userGetJoinedEvents(user_id){
+    userGetJoinedEvents(username){
         //get
-        return JSON.stringify(await this.connectAndRun(db => db.any("SELECT * FROM events WHERE event_id IN (SELECT event_id FROM joined_events WHERE user_id = $1);",[user_id])));
+        return JSON.stringify(await this.connectAndRun(db => db.any("SELECT * FROM events WHERE event_id IN (SELECT event_id FROM joined_events WHERE username = $1);",[username])));
         const events = [
             {"eventid":5,"owner":"Prateek","title":"311 HW2","date":"11/21/20","time":"4:00PM","location":" Dubois library","description":"Doing the HW2 homework","capacity":"7/10"},
             {"eventid":6,"owner":"Aidan","title":" Soccer ","date":"11/26/20","time":"4:20PM","location":"East Soccer Fields","description":"5v5 pickup soccer game","capacity":"6/10"},
