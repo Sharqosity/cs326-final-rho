63b5db1ff17194f92059051f4cc601122f3abc18
Prateek Agarwal <prateekagarwal26@gmail.com>
2020-11-20 02:30:50 -0500

    added console logfor events

--- client/mapPage.js
+++ client/mapPage.js
@@ -18,6 +18,7 @@ function createMap(){
         fetch(feedurl)
         .then(response=> response.json())
         .then(data=> {
+            console.log("And the data is");
             console.log(data);
             for(let i=0;i<data.length;i++){
                 const title= data[i].title;
