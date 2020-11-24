7f8bbc81b6712385b31d4590a2536c861da1a377
Sharqosity <typingpalguy@gmail.com>
2020-11-24 05:45:39 -0500

    code for login input checks once we switch to using js for form submission

--- client/login.js
+++ client/login.js
@@ -3,9 +3,41 @@
 /*
 function loginSetup() {
     document.getElementById("loginButton").addEventListener('click', () => {
+
+
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+        //Check inputs
+        if (username.length === 0) {
+            const alert = document.getElementById('errorAlert');
+            alert.innerHTML = '';
+            alert.classList.add('alert');
+            alert.classList.add('alert-danger');
+            alert.setAttribute('role', 'alert');
+            alert.innerHTML = 'Please enter a username!';
+            return;
+        } else if (password.length === 0) {
+            const alert = document.getElementById('errorAlert');
+            alert.innerHTML = '';
+            alert.classList.add('alert');
+            alert.classList.add('alert-danger');
+            alert.setAttribute('role', 'alert');
+            alert.innerHTML = 'Please enter a password!';
+            return;
+        } else if (username.length > 32) {
+            const alert = document.getElementById('errorAlert');
+            alert.innerHTML = '';
+            alert.classList.add('alert');
+            alert.classList.add('alert-danger');
+            alert.setAttribute('role', 'alert');
+            alert.innerHTML = 'Username must not exceed 32 characters.';
+            return;
+        }
+
         const body = {
-            username: document.getElementById("username").value,
-            password: document.getElementById("password").value
+            username: username,
+            password: password
         };
 
 
